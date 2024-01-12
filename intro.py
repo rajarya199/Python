3ad8a6a53546823be59cba8961420e4644f0899e
rajarya199 <aryalrajan078@gmail.com>
2024-01-12 22:13:32 +0545

    data type intro

---
+++ intro.py
@@ -0,0 +1,21 @@
+print('welcome to butwal')
+# data type
+#integer(int )--> 12,-12
+# float -> 12.34
+#string (str)->'hello', '20' 
+#list -->[1,2,3,4]-- array -dynamic value-mutable value
+#tuple->(2,3,4)-- value are static -immmutable value
+#dictionary ->{'key':'value','key':'value'}
+#boolean->True,False
+#set-> {1,3,4,10} value print in random- not in order
+#set of math
+
+#variable- container to store values
+x=20
+print (x)
+#format with string -->print string with variable value
+print(f'the value of x is {x}')
+print('the value of x is',x)
+print ('the value of x is {}'.format(x))
+
+print(type(x))
