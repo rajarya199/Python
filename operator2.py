ec35b200ae39ad34c1286a72ee00f2b5837e6871
rajarya199 <aryalrajan078@gmail.com>
2024-01-20 23:35:43 +0545

    membership operator

--- operator2.py
+++ operator2.py
@@ -1,1 +1,8 @@
+#membership operator 
+# in operator
+fruits=['orange','mango','guava','banana','apple','mango']
+print('mango' in fruits)# true
+print('x' in fruits) # false
 
+#not in
+print('guava' not in fruits) #false 
